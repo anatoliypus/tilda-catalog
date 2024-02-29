3a6a767a25cab2a5a2a6ed6cedf85395933795b7
Anatoliy <anatoliypuss5@gmail.com>
2024-02-29 18:40:05 +0300

    1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import Catalog from './components/Catalog/Catalog';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+let blockId = 'root'
+eval(`
+  if (typeof CATALOG_PARAMS !== 'undefined' && CATALOG_PARAMS && "blockId" in CATALOG_PARAMS) blockId = CATALOG_PARAMS.blockId
+`)
+
+const root = ReactDOM.createRoot(document.getElementById(blockId));
 root.render(
   <React.StrictMode>
     <Catalog />
